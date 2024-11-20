f294f37bb014eb3176c5e1839a0d050c85150c8a
Omi Gonzalez <omigonzalezamengual@gmail.com>
2024-11-20 09:25:48 +0100

    segundo comit

---
+++ application/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def create_app():
+    """Contructor del núcleo de la aplicación"""
+    app = Flask( __name__ , instance_relative_config=False)
+    db.init_app(app)
+    app.config.from_object('config.Config')
+
+    with app.app_context():
+        # imports
+        from . import routes
+
+        # crea las tablas del modelo
+        db.create_all()
+
+        return app
