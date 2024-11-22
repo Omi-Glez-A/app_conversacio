8a60fd6ef83e1aa7e75f515290829c532c02146e
Omi Gonzalez <omigonzalezamengual@gmail.com>
2024-11-22 11:51:36 +0100

    add forms

---
+++ application/forms.py
@@ -0,0 +1,46 @@
+from flask_wtf import FlaskForm
+from wtforms import (
+    StringField,
+    BooleanField,
+    DateTimeField,
+    SubmitField,
+    PasswordField,
+    SelectField
+)
+from wtforms.validators import DataRequired, Email, EqualTo, Length
+
+class SignUpForm(FlaskForm):
+    username = StringField(
+        'Name',
+        [DataRequired()]
+    )
+    email = StringField(
+        'Email',
+        [
+            Email(message='Please, enter valid email address.'),
+            DataRequired()]
+    )
+    password = PasswordField(
+        'Password',
+        [
+            DataRequired(message='Please enter a password.')
+        ]
+    )
+    confirmPassword = PasswordField(
+        'Confirm Password',
+        [
+            EqualTo(password, message='Passwords must match.')
+        ]
+    )
+    created = DateTimeField(
+        'Created'
+    )
+    admin = BooleanField(
+        'Admin',
+        [DataRequired()],
+        choices=[
+            ('Yes'),
+            ('No')
+        ]
+    )
+    submit = SubmitField('Signup!')
--- application/routes.py
+++ application/routes.py
@@ -1,7 +1,8 @@
-from flask import request, render_template, make_response
+from flask import request, render_template, make_response, redirect, url_for
 from datetime import datetime as dt
 from flask import current_app as app
 from .models import db, User
+from forms import SignUpForm
 
 @app.route('/', methods=['GET'])
 def create_user():
@@ -9,6 +10,9 @@ def create_user():
     username = request.args.get('user')
     email = request.args.get('email')
     if username and email:
+        existing_user = User.query.filter(User.username == username or User.email == email).first()
+        if existing_user:
+            return make_response(f"{username} ({email}) already created!")
         new_user = User(
             username=username,
             email=email,
@@ -17,4 +21,18 @@ def create_user():
         ) # Create an instance of the User class
         db.session.add(new_user) # Adds new User record to database
         db.session.commit() # Commits all changes
-    return make_response(f"{new_user} successfully created!")
+    
+    users = User.query.all()
+    return render_template(
+        'users.html',
+        users=users,
+        title="Show Users"
+    )
+
+@app.route('/signup', methods=('GET', 'POST'))
+def signup():
+    title='Sign Up Form'
+    form = SignUpForm()
+    if form.validate_on_submit():
+        return redirect(url_for('success'))
+    return render_template('signup.html', form=form, title=title)
